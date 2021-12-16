fb2269f049c1bdf0a004cc51533920097efd0ff5
Stas Polishchuk <staspol216@gmail.com>
2021-12-16 13:48:05 +0300

    fixes

--- dist/main.js
+++ dist/main.js
@@ -10,3 +10,30 @@ const swiper = new Swiper('.swiper', {
     },
 
 });
+
+function tab() {
+    let tabBtn = document.querySelectorAll(".tabs-nav__btn");
+    let tabContent = document.querySelectorAll(".catalog__content");
+    let tabName;
+
+    tabBtn.forEach(item => {
+        item.addEventListener("click", selectTabBtn)
+    });
+
+    function selectTabBtn() {
+        tabBtn.forEach(item => {
+            item.classList.remove("is-active");
+        });
+        this.classList.add("is-active");
+        tabName = this.getAttribute('data-tab-name');
+        selectTabContent(tabName);
+    }
+
+    function selectTabContent(tabName) {
+        tabContent.forEach(item => {
+            item.classList.contains(tabName) ? item.classList.add("is-active") : item.classList.remove("is-active")
+        })
+    }
+}
+
+tab();
